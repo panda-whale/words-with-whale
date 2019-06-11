626ec4d5252a528c346f02703e927f6676473768
Mike Huynh <hades@Mikes-MacBook-Pro.local>
2019-06-11 12:14:02 -0700

    re centered star

--- client/components/Row.js
+++ client/components/Row.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import Tile from './Tile';
+import React from "react";
+import Tile from "./Tile";
 
-const Row = (props) => {
-    let tiles = [];
-    
-    for(let i = 0; i < props.row.length; i++) {
-        // const id = `${props.id}, ${i}`
-        const rowId = props.id;
-        const columnId = i
-        tiles.push(< Tile row={rowId} column={columnId} tile={props.row[i]} />)
-    }
-  
-    return (
-        <div>
-            {tiles}
-        </div>
-    )
-}
+const Row = props => {
+  let tiles = [];
+
+  for (let i = 0; i < props.row.length; i++) {
+    // const id = `${props.id}, ${i}`
+    const rowId = props.id;
+    const columnId = i;
+    tiles.push(<Tile row={rowId} column={columnId} tile={props.row[i]} />);
+  }
+
+  return <div>{tiles}</div>;
+};
 
 export default Row;
