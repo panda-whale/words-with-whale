c008a84a53e45b277f0801f8c3b38fd9434ba30e
RadicalRoy <rolandquintana1991@gmail.com>
2019-06-13 14:38:23 -0700

    fixed yo mistakes Jay

--- client/components/Row.js
+++ client/components/Row.js
@@ -3,18 +3,19 @@ import Tile from "./Tile";
 
 const Row = (props) => {
     let tiles = [];
-    
+
     for(let i = 0; i < props.row.length; i++) {
         // const id = `${props.id}, ${i}`
         const rowId = props.id;
         const columnId = i
-        tiles.push(< Tile row={rowId} column={columnId} tile={props.row[i].letter} boardPlace={props.boardPlace} color={props.color} />)
+        tiles.push(< Tile rowId={rowId} columnId={columnId} tile={props.row[i].letter} boardPlace={props.boardPlace} color={props.color} />)
     }
 
     return (
         <div className="cell">
             {tiles}
         </div>
+      
     )
 }
 
